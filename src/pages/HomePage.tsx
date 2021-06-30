cc7571c2f6f88c1d55c9bdb3e25f03db2e0fc632
alexmww <mwwalex@gmail.com>
2021-06-30 20:08:58 +0700

    add state management;

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,4 +1,10 @@
 import React from 'react';
 import { TestComponent } from '../components/TestComponent';
+import { Counter } from '../components/Counter';
 
-export const HomePage: React.FC = () => <TestComponent name={`Hello world!`} />;
+export const HomePage: React.FC = () => (
+  <div>
+    <TestComponent name={`Hello world!`} />
+    <Counter />
+  </div>
+);
